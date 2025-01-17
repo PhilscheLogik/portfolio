235451988a5a89d1049d32f22246514d8da4bf95
Philipp Franke <Franke.Phil89@googlemail.com>
2025-01-17 17:58:52 +0100

    small logo adjusted; button inserted at the bottom; LinkedIn button added;

--- src/app/main-page/profile-card/profile-card.component.ts
+++ src/app/main-page/profile-card/profile-card.component.ts
@@ -5,21 +5,25 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+  styleUrl: './profile-card.component.scss',
 })
 export class ProfileCardComponent {
-
   showImage: boolean;
   showGithub: boolean;
   showMail: boolean;
+  showButton: boolean;
+  showLinkedIn: boolean;
+  showLogo: boolean;
 
-
-
-  constructor(){
+  constructor() {
     this.showImage = false;
+
     this.showGithub = false;
     this.showMail = false;
+    this.showLinkedIn = false;
+
+    this.showLogo = false;
+
+    this.showButton = false;
   }
-
-
 }
