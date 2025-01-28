cd7deb5a2439986e9d37b3bebfab84f52c54bd87
Philipp Franke <Franke.Phil89@googlemail.com>
2025-01-28 14:44:03 +0100

    Contact Desktop Design ready; ref. customised

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,4 +1,8 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
 import {
   TranslatePipe,
   TranslateDirective,
@@ -8,10 +12,13 @@ import {
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [TranslatePipe, TranslateDirective, CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  isChecked: boolean = false;
+
 
 }
