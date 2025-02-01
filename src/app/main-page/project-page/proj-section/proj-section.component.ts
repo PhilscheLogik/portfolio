f4ef9cd3cc8dd681ee0ad89b478da7f6a964b3ac
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-01 20:30:11 +0100

    proj-section test added; project-page navbar created

--- src/app/main-page/project-page/proj-section/proj-section.component.ts
+++ src/app/main-page/project-page/proj-section/proj-section.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-proj-section',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './proj-section.component.html',
-  styleUrl: './proj-section.component.scss'
+  styleUrl: './proj-section.component.scss',
 })
 export class ProjSectionComponent {
-
+  @Input() project!: { title: string; description: string };
 }
--- src/app/main-page/project-page/project-page.component.ts
+++ src/app/main-page/project-page/project-page.component.ts
@@ -15,4 +15,41 @@ import { ProjSectionComponent } from './proj-section/proj-section.component';
 })
 export class ProjectPageComponent {
 
+  selectedProjectIndex = 0;
+  projects: any[] = [];
+
+  constructor(private translate: TranslateService) {
+    this.loadTranslations();
+    
+    // Falls sich die Sprache ändert, müssen wir die Projekte neu übersetzen
+    this.translate.onLangChange.subscribe(() => {
+      this.loadTranslations();
+    });
+  }
+
+  loadTranslations(): void {
+    this.projects = [
+      {
+        title: this.translate.instant('proj.title'),
+        description: this.translate.instant('proj.textLol'),
+      },
+      {
+        title: this.translate.instant('project.title2'),
+        description: this.translate.instant('project.desc2'),
+      },
+      {
+        title: this.translate.instant('project.title3'),
+        description: this.translate.instant('project.desc3'),
+      },
+      {
+        title: this.translate.instant('project.title4'),
+        description: this.translate.instant('project.desc4'),
+      },
+    ];
+  }
+
+  selectProject(index: number): void {
+    this.selectedProjectIndex = index;
+  }
+
 }
