202abe55f425fba4823510dc6f9048ad49660bc9
Philipp Franke <Franke.Phil89@googlemail.com>
2025-04-14 17:57:46 +0200

    Change of the email; small adjustment for the notification on successful sending

--- src/assets/php/sendMail.php
+++ src/assets/php/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->msg;
     
-            $recipient = 'Franke.Phil89@googlemail.com';  
+            $recipient = 'kontakt@franke-philipp.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
