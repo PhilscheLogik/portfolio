4378aa83a1f0b97fc0e5db48144e81d1efc4bcff
Philipp Franke <Franke.Phil89@googlemail.com>
2025-08-08 13:16:44 +0200

    Zoom button displays desired text in References

--- src/app/main-page/references/references.component.ts
+++ src/app/main-page/references/references.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [TranslatePipe],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
 })
-export class ReferencesComponent {}
+export class ReferencesComponent {
+  text = '';
+  constructor(private translate: TranslateService) {}
+
+  // changeText(data:string){
+  //   this.text = data;
+  // }
+
+  changeText(key: string) {
+    if (key == '') {
+      this.text = '';
+    } else {
+      this.translate.get(key).subscribe((translatedText: string) => {
+        this.text = translatedText;
+      });
+    }
+  }
+}
